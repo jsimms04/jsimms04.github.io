304451fd9edb9cc404fee6036a9aacf88670e292
jsimms04 <jalensimms59@gmail.com>
2022-01-26 14:04:06 +0000

    <message about commit>

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,8 +37,11 @@
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
 
         // example:
-        createPlatform(400, 250);
         createPlatform(170, 460, 0.5);
+
+        createPlatform(400, 330);
+        createPlatform(170, 460, 0.5) 
+        createPlatform(500, 555)
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
